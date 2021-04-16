66f844fa1c9e4c216d0921ca183d8b043866f8a9
Shalini8 <shalu8mar@gmail.com>
2021-04-16 19:56:42 +0530

    [Add]: EmailId

--- src/userdata/Main.java
+++ src/userdata/Main.java
@@ -3,6 +3,6 @@ package userdata;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration Portal");
-		Registration.checkLastName();
+		Registration.checkEmailID();
 	}
 }
--- src/userdata/Registration.java
+++ src/userdata/Registration.java
@@ -6,14 +6,16 @@ import java.util.regex.Pattern;
 public class Registration {
 	static Scanner userInput = new Scanner(System.in);
 	
-	//*To check the Format of last name*
-	public static void checkLastName() {
-		String LastName = "^[A-Z]{1}[a-z]{2,}$";
-		System.out.println("Enter the Last Name: ");
+	//*To check the Format of EmailID*
+	public static void checkEmailID() { // UC3
+		System.out.println("Enter the valid EmailId: ");
 		String check = userInput.next();
-		if (Pattern.matches(LastName, check))
-			System.out.println("Enter the LastName in correct Format: ");
+		String emailId = "^[a-zA-Z0-9]{1,}[.a-zA-Z0-9]*@[a-zA-Z0-9]{1,}.[a-z]{1,3}(.[a-z]{2})*$";
+		if (Pattern.matches(emailId, check))
+			System.out.println("Correct Format of email: ");
 		else
-			System.out.println("Invalid way : please try again!!!");
+			System.out.println("Invalid email  : please try again!!!");
 	}
+
+	
 }
